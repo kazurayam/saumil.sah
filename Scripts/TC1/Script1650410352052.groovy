0adc6df5d46234907156373e250b8bf6de187ac0
kazurayam <kazuaki.urayama@gmail.com>
2022-04-20 10:22:15 +0900

    update

--- Scripts/TC1/Script1650410352052.groovy
+++ Scripts/TC1/Script1650410352052.groovy
@@ -1,24 +1,35 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
+
 
 File html = new File("Unified Lead_ 201481098, TA_UL_FU_OPP_HARMONY_ME_DIRECT, Test Company RD.05 - [SAP].mhtml")
-URL url = html.toURL();
+URL url = html.toURI().toURL();
 WebUI.openBrowser(url.toExternalForm());
-WebUI.delay(3)
+
+
+WebUI.switchToFrame(makeTO("//iframe[@id='CRMApplicationFrame']"), 5)
+WebUI.switchToFrame(makeTO("//frame[@id='WorkAreaFrame1']"), 5)
+
+List<String> candidates = [
+	"/html/body[@role='main']",
+	"//form[@id='myFormId']",
+	"//table[@id='th-l-workAreaMainTable']",
+	"//table[@id='th-l-workAreaMainTable']//div[@id='thtmlbOverviewPageBox']",
+	"//table[@id='th-l-workAreaMainTable']//div[@id='thtmlbOverviewPageBox']//thtmlb:grid",
+	"//table[@id='th-l-workAreaMainTable']//grid[1]/gridcell[1]",
+	"//table[@id='th-l-workAreaMainTable']//tr[contains(text(),'General Data')]"
+	];	
+for (String locator in candidates) {	
+	TestObject to_leadId = makeTO(locator);
+	WebUI.verifyElementPresent(to_leadId, 5)
+}
+
 WebUI.closeBrowser();
+
+
+TestObject makeTO(String expr) {
+	TestObject to = new TestObject(expr);
+	to.addProperty("xpath", ConditionType.EQUALS, expr)
+	return to
+}
