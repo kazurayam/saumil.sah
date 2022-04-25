66010c02f2e3142c47849f8e0acae3f30af56bd6
kazurayam <kazuaki.urayama@gmail.com>
2022-04-25 22:06:53 +0900

    updates

--- Scripts/TC4b/Script1650891274724.groovy
+++ Scripts/TC4b/Script1650891274724.groovy
@@ -9,15 +9,23 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 File html = new File("Unified Lead_ 201481098, TA_UL_FU_OPP_HARMONY_ME_DIRECT, Test Company RD.05 - [SAP].mhtml")
 URL url = html.toURI().toURL();
 WebUI.openBrowser(url.toExternalForm());
-
+WebUI.delay(1)
 WebDriver myDriver = DriverFactory.getWebDriver()
 
 CRMApplicationFrame = myDriver.findElement(By.id("CRMApplicationFrame"))
 myDriver.switchTo().frame(CRMApplicationFrame)
 
-WorkAreaFrame1 = myDriver.findElement(By.id("WorkAreaFrame1"))
-myDriver.switchTo().frame(WorkAreaFrame1)
+frameset = myDriver.findElement(By.tagName("frameset"))
+//c = frameset.getAttribute("rows")
 
+contentFrameset = frameset.findElement(By.id("contentFrameset"))
+//d = content.getAttribute("rows")
+
+WorkAreaFrame1 = contentFrameset.findElement(By.id("WorkAreaFrame1"))
+e = WorkAreaFrame1.getAttribute("name")
+println e
+
+myDriver.switchTo().frame(WorkAreaFrame1)
 WebElement el = myDriver.findElement(By.xpath("//td/div/span/label[normalize-space(.) = 'ID:']/parent::span/parent::div/parent::td/following-sibling::td[1]//span"))
 String val = el.getText()
 
